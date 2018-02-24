8a3a84d53afcd01749b28c61dd7845854369c585
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-02-24 16:18:35 -0500

    Build component to render list of cakes - add api keys to config file - try to fetch data from edamam

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const CakeList = ({cakes}) => {
+  return(
+    <ul>
+      {
+        cakes.map((cake) => <li key={cake}>{cake}</li>)
+      }
+    </ul>
+  );
+};
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      cakes: []
+    }
+  }
+
+  fetchRecipes = () => {
+    fetch('https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=431230dd&app_key=064d85707c2fd24bca1422720f7a9274', {
+  	// method: 'POST',
+    mode: 'no-cors',
+  	headers: {
+		'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then(recipes => {
+      console.log('fetched ', recipes);
+      this.setState({
+        cakes: recipes
+      });
+    });
+  };
+
+  componentDidMount(){
+    this.fetchRecipes();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CakeList cakes={this.state.cakes}/>
       </div>
     );
   }
