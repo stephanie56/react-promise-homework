f1a62971cb6f8cd97e4d290d1560f55455a31d9c
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-02-26 15:33:52 -0500

    Render a list of cakes and its calories

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 const CakeList = ({cakes}) => {
+  console.log(cakes);
   return(
     <ul>
       {
-        cakes.map((cake) => <li key={cake}>{cake}</li>)
+        cakes.map(({name, calories}, idx) => <li key={idx}>{name}: {calories}</li>)
       }
     </ul>
   );
 };
+
+
 
 class App extends Component {
   constructor(){
@@ -21,13 +24,18 @@ class App extends Component {
   }
 
   fetchRecipes = () => {
-    fetch('https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_KEY}')
+    const APP_ID = '9f1497b3';
+    const APP_KEY = 'd489d7c7d4bec18f15a54e7467e0e936	';
+    fetch(`https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_KEY}`)
     .then(res => res.json())
     .then(recipes => {
       const data = recipes.hits.map(({recipe}) => {
         return Object.assign({}, {name: recipe.label, calories: Math.floor(recipe.calories)});
       });
       console.log('Fecthing...', data);
+      this.setState({
+        cakes: data
+      });
     });
   };
 
@@ -38,7 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CakeList cakes={this.state.cakes}/>
+        <CakeList cakes={this.state.cakes} />
       </div>
     );
   }
